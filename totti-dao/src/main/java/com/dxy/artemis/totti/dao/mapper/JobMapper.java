8073c35b22bc8107aa43422777abb94dc00e8251
gavindu0705 <gavindu0705@gmail.com>
2019-11-07 23:11:19 +0800

    mapper

---
+++ totti-dao/src/main/java/com/dxy/artemis/totti/dao/mapper/JobMapper.java
@@ -0,0 +1,36 @@
+package com.dxy.artemis.totti.dao.mapper;
+
+import com.dxy.artemis.totti.dao.pojo.Job;
+import com.dxy.artemis.totti.dao.pojo.JobExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface JobMapper {
+    long countByExample(JobExample example);
+
+    int deleteByExample(JobExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Job record);
+
+    int insertSelective(Job record);
+
+    List<Job> selectByExampleWithBLOBs(JobExample example);
+
+    List<Job> selectByExample(JobExample example);
+
+    Job selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Job record, @Param("example") JobExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Job record, @Param("example") JobExample example);
+
+    int updateByExample(@Param("record") Job record, @Param("example") JobExample example);
+
+    int updateByPrimaryKeySelective(Job record);
+
+    int updateByPrimaryKeyWithBLOBs(Job record);
+
+    int updateByPrimaryKey(Job record);
+}
